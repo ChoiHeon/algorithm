513a3dae55b17b59cd2e5ca29b009cdf99d760e3
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-03-04 18:31:43 +0900

    Upload algorithm solutions

---
+++ 03_프로그래머스/스킬트리.cpp
@@ -0,0 +1,46 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/49993
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int solution(string skills, vector<string> skill_trees) {
+	int answer = 0;
+	map<char, int> m;
+	set<char> s;
+
+	for (int i = 0; i < skills.size(); i++) {
+		m.insert(make_pair(skills[i], i));
+		s.insert(skills[i]);
+	}
+
+	for (auto skill_tree : skill_trees) {
+		int cnt = 0;
+
+		for (auto skill : skill_tree)
+			if (s.find(skill) != s.end())
+				if (cnt == m[skill])
+					cnt++;
+				else
+					cnt = -1;
+
+		if (cnt != -1)
+			answer++;
+	}
+
+	return answer;
+}
+
+
+// test
+int main() {
+	string arg1 = "CBD";
+	vector<string> arg2{ "BACDE", "CBADF", "AECB", "BDA" };
+	
+	cout << solution(arg1, arg2);	// expected output: 2
+}
---
+++ 03_프로그래머스/주식가격.cpp
@@ -0,0 +1,32 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/42584
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer;
+
+	for (int i = 0; i < prices.size(); i++) {
+		int term = 0;
+
+		for (int j = i + 1; j < prices.size(); j++) {
+			term++;
+
+			if (prices[j] < prices[i])
+				break;
+		}
+
+		answer.push_back(term);
+	}
+
+	return answer;
+}
+
+// test
+int main() {
+
+	return 0;
+}
