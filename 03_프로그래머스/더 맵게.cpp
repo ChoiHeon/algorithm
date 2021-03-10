1736290417a3a5c24091abbe35173ba4a1fd997b
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-03-10 15:24:08 +0900

    Upload codes

---
+++ 03_프로그래머스/더 맵게.cpp
@@ -0,0 +1,45 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/42626
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define heapify(v) make_heap(v.begin(), v.end(), greater<int>())
+#define heappop(v) pop_heap(v.begin(), v.end(), greater<int>())
+#define heappush(v) push_heap(v.begin(), v.end(), greater<int>())
+
+int solution(vector<int> v, int k) {
+	int answer = 0;
+	
+	make_heap(v.begin(), v.end(), greater<int>());
+
+	while (v.front() < k) {
+		int e1, e2, e3;
+
+		e1 = v.front();
+		heappop(v);
+		v.pop_back();
+
+		e2 = v.front();
+		heappop(v);
+		v.pop_back();
+
+		e3 = e1 + 2 * e2;
+		v.push_back(e3);
+		heappush(v);
+
+		answer++;
+	}
+
+	return answer;
+}
+
+
+// test
+int main() {
+
+}
---
+++ 03_프로그래머스/소수 찾기.cpp
@@ -0,0 +1,53 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/42839
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+bool prime(int n) {
+	if (n <= 1)
+		return false;
+
+	int k = sqrt(n);
+
+	for (int i = 2; i <= k; i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
+
+int solution(string numbers) {
+	int answer = 0;
+	int n = numbers.size();
+	unordered_set<int> s;
+
+	sort(numbers.begin(), numbers.end());
+
+	do {
+		string sub = "";
+
+		for (int i = 0; i < n; i++) {
+			sub += numbers[i];
+			s.insert(stoi(sub));
+		}
+
+	} while (next_permutation(numbers.begin(), numbers.end()));
+
+	for (auto i : s)
+		if (prime(i))
+			answer++;
+
+	return answer;
+}
+
+
+// test
+int main() {
+	cout << prime(97) << endl;
+}
