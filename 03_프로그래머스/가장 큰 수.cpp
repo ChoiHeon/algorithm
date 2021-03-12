974e4a192d386111353de78f63931e3283849ef3
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-03-12 15:49:01 +0900

    Upload codes

---
+++ 03_프로그래머스/가장 큰 수.cpp
@@ -0,0 +1,52 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/42746
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int compare(string n1, string n2) {
+	int min_size = min(n1.size(), n2.size());
+	int ret;
+
+	for (int i = 0; i < min_size; i++) {
+		ret = n1[i] > n2[i];
+
+		if (ret != 0)
+			return ret;
+	}
+
+	return n1.size() > n2.size();
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	vector<string> numbers2str;
+
+	for (int num : numbers)
+		numbers2str.push_back(to_string(num));
+
+	sort(numbers2str.begin(), numbers2str.end(), compare);
+
+	for (auto num : numbers2str)
+		answer += num;
+
+	for (int i = 0; i < answer.size(); i++) {
+		if (answer[i] != '0' || i == answer.size() - 1) {
+			answer = answer.substr(i);
+			break;
+		}
+	}
+
+	return answer;
+}
+
+
+// test
+int main() {
+
+}
---
+++ 03_프로그래머스/큰 수 만들기.cpp
@@ -0,0 +1,34 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/42860
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string number, int k) {
+	string answer = "";
+	int n = number.size();
+	int s = 0;
+
+	for (int i = 0; i < n - k; i++) {
+		char max_value = number[s];
+
+		for (int j = s + 1; j <= i + k; j++)
+			if (max_value < number[j]) {
+				max_value = number[j];
+				s = j;
+			}
+
+		answer += max_value;
+		s++;
+	}
+
+	return answer;
+}
+
+// test
+int main() {
+}
