ecd8ec1ed0799491fe265902df1269e80c88663c
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-08-01 19:48:27 +0900

    Upload codes

---
+++ 02_백준/[1948] 임계경로.cpp
@@ -0,0 +1,74 @@
+﻿// 문제: https://www.acmicpc.net/problem/1948
+
+#include <iostream>
+#include <memory.h>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int s, e;
+int road_cnt;
+int dist[10001];
+bool visited[10001];
+vector<vector<int>> path;
+vector<vector<pair<int, int>>> graph;
+
+void searchPath(int prev, int crnt, int d) {
+	if (d < dist[crnt])
+		return;
+	if (d != 0 && d == dist[crnt]) {
+		path[crnt].emplace_back(prev);
+		return;
+	}
+
+	dist[crnt] = d;
+	path[crnt].clear();
+	path[crnt].emplace_back(prev);
+
+	for (auto& info : graph[crnt]) {
+		int next = info.first;
+		int c = info.second;
+
+		searchPath(crnt, next, c + d);
+	}
+}
+
+void trackPath(int v) {
+	visited[v] = true;
+
+	for (int u : path[v]) {
+		road_cnt++;
+
+		if (!visited[u])
+			trackPath(u);
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+	scanf("%d", &m);
+
+	memset(dist, 0, sizeof(dist));
+	memset(visited, false, sizeof(visited));
+
+	path.assign(n + 1, vector<int>());
+	graph.assign(n + 1, vector<pair<int, int>>());
+
+	for (int v, u, c, i = 0; i < m; i++) {
+		scanf("%d %d %d", &v, &u, &c);
+		graph[v].push_back({ u, c });
+	}
+
+	scanf("%d %d", &s, &e);
+
+	road_cnt = -1;
+
+	searchPath(0, s, 0);
+	trackPath(e);
+
+	printf("%d\n", dist[e]);
+	printf("%d\n", road_cnt);
+
+	return 0;
+}
---
+++ 02_백준/[2042] 구간 합 구하기.cpp
@@ -0,0 +1,98 @@
+﻿// 문제: https://www.acmicpc.net/problem/2042
+
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+const int MAX_N = 1000000;
+const int TREE_SIZE = (1 << (20 + 1)) + 1;
+
+int n, m, k;
+ll arr[MAX_N];		// 시작 인덱스 = 0
+ll tree[TREE_SIZE];	// 시작 인덱스 = 1
+
+
+/*
+node = 세그먼트 트리의 노드
+left, right = 배열의 범위
+*/
+ll init(int node, int left, int right) {
+	if (left == right)
+		return tree[node] = arr[left];
+
+	int mid = (left + right) / 2;
+	
+	return tree[node] = init(node * 2, left, mid) + init(node * 2 + 1, mid + 1, right);
+}
+
+
+/*
+node = 세그먼트 트리의 노드
+index = 배열의 값을 변경할 인덱스
+diff = 기존의 값과 변경할 값의 차이
+left = 배열의 범위
+*/
+void update(int node, int left, int right, int index, ll diff) {
+	if (index < left || right < index)
+		return;
+
+	tree[node] += diff;
+
+	if (left != right) {
+		int mid = (left + right) / 2;
+		
+		update(node * 2, left, mid, index, diff);
+		update(node * 2 + 1, mid + 1, right, index, diff);
+	}
+}
+
+
+/*
+node = 세그먼트 트리의 노드
+left, right = 현재 노드가 가리키는 배열의 범위
+start, end = 합을 구하려는 배열의 범위
+*/
+ll sum(int node, int left, int right, int start, int end) {
+	if (end < left || right < start)
+		return 0LL;
+
+	if (start <= left && right <= end)
+		return tree[node];
+
+	int mid = (left + right) / 2;
+
+	return sum(node * 2, left, mid, start, end) + sum(node * 2 + 1, mid + 1, right, start, end);
+}
+
+void print() {
+	for (int i = 0; i < 13; i++)
+		cout << tree[i] << " ";
+	cout << endl;
+}
+
+int main() {
+	scanf("%d %d %d", &n, &m, &k);
+
+	for (int i = 0; i < n; i++) 
+		scanf("%lld", &arr[i]);
+
+	init(1, 0, n - 1);
+
+	int t = m + k;
+	int a, b;
+	ll c;
+
+	while (t--) {
+		scanf("%d %d %lld", &a, &b, &c);
+
+		if (a == 1) {
+			update(1, 0, n - 1, b - 1, c - arr[b - 1]);
+			arr[b - 1] = c;
+		}
+		else
+			printf("%lld\n", sum(1, 0, n - 1, b - 1, c - 1));
+	}
+
+	return 0;
+}
