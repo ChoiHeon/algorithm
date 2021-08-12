e22e6a1076db32455dfb0554e401295b4d28d79c
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-08-12 15:41:47 +0900

    Upload cods

---
+++ 02_백준/[14267] 회사 문화 1.cpp
@@ -0,0 +1,51 @@
+﻿// 문제: https://www.acmicpc.net/problem/14267
+
+#include <iostream>
+#include <memory.h>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int answer[100000];
+vector<vector<int>> junior;
+
+
+void solve(int i, int w) {
+	answer[i] += w;
+
+	for (int j : junior[i])
+		solve(j, answer[i]);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n >> m;
+
+	memset(answer, 0, sizeof(answer));
+	junior.assign(n, vector<int>());
+
+	int i, w;
+	cin >> i;
+
+	for (int j = 1; j < n; j++) {
+		cin >> i;
+		junior[--i].push_back(j);
+	}
+
+	while (m--) {
+		cin >> i >> w;
+		answer[--i] += w;
+	}
+
+	solve(0, 0);
+
+	for (i = 0; i < n; i++)
+		cout << answer[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
---
+++ 02_백준/[2618] 경찰차.cpp
@@ -0,0 +1,85 @@
+﻿// 문제: https://www.acmicpc.net/problem/2618
+
+#include <iostream>
+#include <memory.h>
+#include <vector>
+
+using namespace std;
+typedef pair<int, int> pii;
+
+int n, m;
+int cache[1001][1001];
+vector<pii> path_A;
+vector<pii> path_B;
+
+inline int min(int i, int j) { return i < j ? i : j; }
+inline int max(int i, int j) { return i > j ? i : j; }
+
+int getMinDistance(int a, int b) {
+	if (a == m || b == m)
+		return 0;
+
+	int& ret = cache[a][b];
+
+	if (ret != -1)
+		return ret;
+
+	ret = 0x7f7f7f7f;
+
+	int max_location = max(a, b) + 1;
+	int dist_A = abs(path_A[max_location].first - path_A[a].first) +
+				 abs(path_A[max_location].second - path_A[a].second);
+	int dist_B = abs(path_B[max_location].first - path_B[b].first) +
+				 abs(path_B[max_location].second - path_B[b].second);
+	int ret1 = getMinDistance(max_location, b) + dist_A;
+	int ret2 = getMinDistance(a, max_location) + dist_B;
+
+	ret = min(ret1, ret2);
+
+	return ret;
+}
+
+void trackPath(int a, int b) {
+	if (a == m || b == m)
+		return;
+
+	int max_location = max(a, b) + 1;
+	int dist_A = abs(path_A[max_location].first - path_A[a].first) +
+				 abs(path_A[max_location].second - path_A[a].second);
+	int dist_B = abs(path_B[max_location].first - path_B[b].first) +
+				 abs(path_B[max_location].second - path_B[b].second);
+	int ret1 = getMinDistance(max_location, b) + dist_A;
+	int ret2 = getMinDistance(a, max_location) + dist_B;
+
+	if (ret1 > ret2) {
+		printf("2\n");
+		trackPath(a, max_location);
+	}
+	else {
+		printf("1\n");
+		trackPath(max_location, b);
+	}
+}
+
+int main() {
+
+	scanf("%d", &n);
+	scanf("%d", &m);
+	memset(cache, -1, sizeof(cache));
+
+	path_A.push_back({ 1, 1 });
+	path_B.push_back({ n, n });
+
+	int x, y;
+
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &x, &y);
+		path_A.push_back({ x, y });
+		path_B.push_back({ x, y });
+	}
+
+	printf("%d\n", getMinDistance(0, 0));
+	trackPath(0, 0);
+
+	return 0;
+}
