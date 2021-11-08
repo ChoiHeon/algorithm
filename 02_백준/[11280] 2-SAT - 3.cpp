9d3a1e62c8e8fbc4f710ebe3f3e04d13d4279994
ChoiHeon <49025156+ChoiHeon@users.noreply.github.com>
2021-11-08 16:57:47 +0900

    Upload codes

---
+++ 02_백준/[11280] 2-SAT - 3.cpp
@@ -0,0 +1,105 @@
+﻿// https://www.acmicpc.net/problem/11280
+
+#include <iostream>
+#include <memory.h>
+#include <vector>
+#include <stack>
+#include <set>
+#include <algorithm>
+
+
+using namespace std;
+
+const int MAX = 20002;
+
+int id = 1;
+int nodeId[MAX];
+bool finished[MAX];
+vector<int> edges[MAX];
+vector<set<int>> SCC;
+stack<int> s;
+
+
+int notX(int x)		{ return x ^ 1; }
+int trueX(int x)	{ return x << 1; }
+int falseX(int x)	{ return x << 1 | 1; }
+
+int dfs(int x) {
+	int parent = ++id;
+	nodeId[x] = parent;
+	s.push(x);
+
+	for (int y : edges[x]) {
+		if (nodeId[y] == 0)
+			parent = min(parent, dfs(y));
+		else if (!finished[y])
+			parent = min(parent, nodeId[y]);
+	}
+
+	if (parent == nodeId[x]) {
+		set<int> scc;
+		
+		while (true) {
+			int t = s.top();  s.pop();
+			scc.insert(t);
+			finished[t] = true;
+			if (t == x)  break;
+		}
+
+		SCC.push_back(scc);
+	}
+
+	return parent;
+}
+
+int two_SAT() {
+	int n, m;
+	cin >> n >> m;
+	memset(nodeId, 0, sizeof(nodeId));
+	memset(finished, 0, sizeof(finished));
+
+	for (int i = 0; i < m; ++i) {
+		int x, y;
+		cin >> x >> y;
+
+		x = x < 0 ? falseX(-x) : trueX(x);
+		y = y < 0 ? falseX(-y) : trueX(y);
+
+		edges[notX(x)].push_back(y);
+		edges[notX(y)].push_back(x);
+	}
+
+	n *= 2;
+	for (int x = 1; x <= n; ++x) {
+		if (!nodeId[x])
+			dfs(x);
+	}
+
+	bool valid = true;
+	for (auto& scc : SCC) {
+		auto end = scc.end();
+
+		for (int x : scc) {
+			if (scc.find(notX(x)) != end) {
+				valid = false;
+				break;
+			}
+		}
+
+		if (!valid)  break;
+	}
+
+	return valid ? 1 : 0;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int answer = two_SAT();
+
+	cout << answer << endl;
+
+	return 0;
+}
---
+++ 03_프로그래머스/01_위클리 챌린지/10_피로도.cpp
@@ -0,0 +1,43 @@
+﻿// https://programmers.co.kr/learn/courses/30/lessons/87946
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int answer;
+
+void travel(int m, int x, const vector<vector<int>>& dungeons, int visited) {
+	bool finish = true;
+
+	for (int i = 0; i < n; i++) {
+		if (!(visited & (1 << i)) && m >= dungeons[i][0]) {
+			finish = false;
+			travel(m - dungeons[i][1], x + 1, dungeons, visited | (1 << i));
+		}
+	}
+
+	if (finish)
+		answer = max(answer, x);
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+	n = dungeons.size();
+	answer = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (k >= dungeons[i][0])
+			travel(k - dungeons[i][1], 1, dungeons, 1 << i);
+	}
+
+	return answer;
+}
+
+
+int main() {
+
+
+	return 0;
+}
